cc2456ae559b4643342a7290e11310b067cd03e9
peel3r <ismc2222@gmail.com>
2016-10-19 23:43:58 +0100

    adds get diary by id from api

--- src/app/shared/manager-api.service.ts
+++ src/app/shared/manager-api.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core'
 import { Http, Response } from '@angular/http';
+
+import 'rxjs';
+import { Observable } from 'rxjs/Observable'
 @Injectable()
 
 export class ManagerApi {
 
   private baseUrl = 'https://cpms-api.herokuapp.com/api'
-
+  currentDiary: any = {};
   constructor(private http: Http) {}
 
 
   getDiaries() {
     return new Promise(resolve => {
       this.http.get(`${this.baseUrl}/diaries`)
-        .subscribe(res => resolve(res))
+        .subscribe(res => resolve(res.json()))
+    })
+  }
+  getDiaryData(diaryId) : Observable<any>  {
+    console.log('***diaryId', diaryId)
+    return this.http.get(`${this.baseUrl}/diaries/${diaryId}`)
+      .map((response: Response) => {
+        this.currentDiary = response.json();
+        return this.currentDiary;
     })
   }
 }
